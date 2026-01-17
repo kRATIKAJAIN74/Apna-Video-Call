4c02a105cbf966c04d978907d2b39367340d1031
Kratika Jain <jkratika8@gmail.com>
2026-01-17 11:01:36 +0530

    front

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
 function LandingPage() {
     return ( 
         <div className='landingPageContainer'>
   <nav>
-    <div className='navHeader'></div>
-    <div className='navlist'></div>
+    <div className='navHeader'><h2>Apna Video Call</h2></div>
+    <div className='navlist'>
+        <p>Join as Guest</p>
+        <p>Register</p>
+       <div role='button'><p>Login</p></div>
+    </div>
     </nav>
+
+    <div class="landingMainContainer">
+        <div><h1><span style={{color: "#FF9839"}}>Connect</span> with your loved Ones</h1>
+        <p>Cover a distance by Apna Video Call</p>
+        <div role='button'> 
+           <Link to={"/home"}>Get Started</Link>
+        </div>
+        </div>
+        
+        <div>
+            <img src="/mobile.png" alt="" />
+        </div>
+    </div>
         </div>
      );
 }
