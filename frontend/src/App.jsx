afe892ad166a886a355ff336fe6c135fc669be15
Vivekchandra20 <3140vivekchandra@gmail.com>
2026-01-22 12:10:32 +0530

    Video and audio permission set & getting video in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
-import {Route,BrowserRouter as Router,Routes} from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import LandingPage from "./pages/landing";
-import Authentication from './pages/authentication';
+import Authentication from "./pages/authentication";
 import { AuthProvider } from "./contexts/AuthContext";
+import VideoMeetComponent from "./pages/VideoMeet";
 
 function App() {
   return (
-   <div className="App">
-  <Router>
-    <AuthProvider>
-    <Routes>
-      {/* <Route path="/home" element= /> */}
-      <Route path="/" element={ <LandingPage/>} />
-      <Route path="/auth" element={<Authentication/>}/>
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Routes>
+            {/* <Route path="/home" element= /> */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={<Authentication />} />
 
-    </Routes>
-    </AuthProvider>
-    </Router>
-   </div>
-    
-    
+            <Route path="/:url" element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,51 +1,50 @@
-import axios from "axios"
+import axios from "axios";
 import { createContext, useState, useContext } from "react";
 import httpStatus from "http-status";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext({});
 const client = axios.create({
-  baseURL: "http://localhost:8000/api/v1/users"
-})
+  baseURL: "http://localhost:8000/api/v1/users",
+});
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
   const authContext = useContext(AuthContext);
-  const [userData, setUserData] = useState(AuthContext);
-  const handleRegister = async(name,username,password) => {
+  const [userData, setUserData] = useState(authContext);
+  const handleRegister = async (name, username, password) => {
     try {
-  let request = await client.post("/register",{
-  name: name,
-  username: username,
-  password: password
-    })
-    if(request.status === httpStatus.CREATED){
-      return request.data.message;
+      let request = await client.post("/register", {
+        name: name,
+        username: username,
+        password: password,
+      });
+      if (request.status === httpStatus.CREATED) {
+        return request.data.message;
+      }
+    } catch (err) {
+      throw err;
     }
-    } catch(err) {
-  throw err;
-    }
-  }
-    const router = useNavigate();
+  };
+  const router = useNavigate();
   const handleLogin = async (username, password) => {
-    try{
-   let request = await client.post("/login",{
-  username: username,
-  password: password
-    })
-    if(request.status === httpStatus.OK){
-      localStorage.setItem("token", request.data.token);
+    try {
+      let request = await client.post("/login", {
+        username: username,
+        password: password,
+      });
+      if (request.status === httpStatus.OK) {
+        localStorage.setItem("token", request.data.token);
+      }
+    } catch (err) {
+      throw err;
     }
-    } catch(err){
-   throw err;
-    }
-  }
-  
+  };
+
   const data = {
-    userData, setUserData, handleRegister, handleLogin,
-  }
-  return(
-    <AuthContext.Provider value={data}>
-    {children}
-    </AuthContext.Provider>
-  )
-}
+    userData,
+    setUserData,
+    handleRegister,
+    handleLogin,
+  };
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
